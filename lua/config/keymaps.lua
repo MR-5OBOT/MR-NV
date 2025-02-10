02031e3018d94612b2c5267de285f5aac6f9f974
MR-5OBOT <yassir7m7@gmail.com>
2025-02-10 12:30:15 +0100

    p

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,24 +11,30 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 -- local map = vim.api.nvim_set_keymap
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-keymap("i", "jk", "<ESC>", opts)                    -- Press jk fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts) -- Press jk fast to exit insert mode
 keymap("n", "<Enter>", "<cmd>nohlsearch<CR>", opts) -- Clear search
-keymap('n', '<TAB>', ':bnext<CR>', { noremap = true, silent = true, desc = 'Next buffer' })
-keymap('n', '<S-TAB>', ':bprevious<CR>', { noremap = true, silent = true, desc = 'Next buffer' })
+keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Next buffer" })
 keymap("n", "<leader>mx", "<cmd>!chmod +x %<CR>", { desc = "Chmod +x without leaving document", silent = true })
 
-keymap('n', '<leader>ms', ':%s/', { desc = "multi select & replace" }) -- Easier multi select and remove
-vim.keymap.set('v', '<leader>ms', [[:s//g<Left><Left>]],
-    { desc = "Multi-select & replace", noremap = true, silent = true })
+keymap("n", "<leader>ms", ":%s/", { desc = "multi select & replace" }) -- Easier multi select and remove
+vim.keymap.set(
+	"v",
+	"<leader>ms",
+	[[:s//g<Left><Left>]],
+	{ desc = "Multi-select & replace", noremap = true, silent = true }
+)
 
-
-keymap('v', 'J', ":m '>+1<CR>gv=gv")                                              -- move selected lines DOWN
-keymap('v', 'K', ":m '<-2<CR>gv=gv")                                              -- move selected lines UP
+keymap("v", "J", ":m '>+1<CR>gv=gv") -- move selected lines DOWN
+keymap("v", "K", ":m '<-2<CR>gv=gv") -- move selected lines UP
 
 keymap("n", "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true }) -- Allow moving the cursor through wrapped lines
 keymap("n", "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true }) -- Allow moving the cursor through wrapped lines
 
-keymap('n', '<leader>e', ':Explore<CR>', { noremap = true, silent = true })
+keymap("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true })
+
+-- Keymap to run the current file (e.g., for a Python script)
+keymap("n", "<leader>r", ":botright split | resize 10 | terminal python %<CR>i", { noremap = true, silent = true })
