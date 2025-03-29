f48f5c12137b95d09a1e89bfee91062314f7d5e3
MR-5OBOT <yassir7m7@gmail.com>
2025-03-29 08:06:20 +0000

    make lsp hover border finally work boooooooooooooooom

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,22 +1,27 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		-- "rcarriga/nvim-notify",
-	},
-	opts = {
-		views = {
-			cmdline_popup = {
-				border = {
-					style = "none",
-					padding = { 1, 1 },
-				},
-				filter_options = {},
-				win_options = {
-					winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-				},
-			},
-		},
-	},
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        -- "rcarriga/nvim-notify",
+    },
+    opts = function(_, opts)
+        -- only way to make lsp hover works for me
+        opts.presets = opts.presets or {}
+        opts.presets.lsp_doc_border = true
+
+        -- style
+        opts.views = {
+            cmdline_popup = {
+                border = {
+                    style = "none",
+                    padding = { 1, 1 },
+                },
+                filter_options = {},
+                win_options = {
+                    winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+                },
+            },
+        }
+    end,
 }
