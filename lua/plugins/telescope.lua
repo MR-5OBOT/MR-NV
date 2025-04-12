cd020ab7b2e512f23968c52d21a53fc7101880cc
MR-5OBOT <yassir7m7@gmail.com>
2025-04-12 17:28:51 +0100

    make teh perfect telescop cfg

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,64 +1,46 @@
-return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    lazy = false,
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    },
-    config = function()
-        local actions = require('telescope.actions')
-        require('telescope').setup({
-            defaults = {
-                file_ignore_patterns = { '.git/', 'node_modules/', '.DS_Store' },
-                layout_config = {
-                    horizontal = {
-                        prompt_position = 'top',
-                        preview_width = 0.6,
-                    },
-                },
-                mappings = {
-                    i = {
-                        ['<C-j>'] = actions.move_selection_next,
-                        ['<C-k>'] = actions.move_selection_previous,
-                        ['<esc>'] = actions.close,
-                    },
-                },
-            },
-            pickers = {
-                find_files = {
-                    hidden = true,
-                    find_command = { 'rg', '--files', '--hidden', '--ignore', '--glob', '!.git/*' },
-                },
-            },
-            extensions = {
-                fzf = {},
-            },
-        })
+---------------------------------------------------------------
+-- ██╗  ██╗███████╗██╗   ██╗███╗   ███╗ █████╗ ██████╗ ███████╗
+-- ██║ ██╔╝██╔════╝╚██╗ ██╔╝████╗ ████║██╔══██╗██╔══██╗██╔════╝
+-- █████╔╝ █████╗   ╚████╔╝ ██╔████╔██║███████║██████╔╝███████╗
+-- ██╔═██╗ ██╔══╝    ╚██╔╝  ██║╚██╔╝██║██╔══██║██╔═══╝ ╚════██║
+-- ██║  ██╗███████╗   ██║   ██║ ╚═╝ ██║██║  ██║██║     ███████║
+-- ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     ╚══════╝
 
-        -- Load fzf extension
-        require('telescope').load_extension('fzf')
+-----------------------------------------------
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+-- local map = vim.api.nvim_set_keymap
 
-        -- Keymaps
-        local builtin = require('telescope.builtin')
+-- now it's used on init.lua
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<space>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-        -- vim.keymap.set('n', '<space>fb', builtin.buffers, { desc = 'Telescope buffers' })
+keymap("i", "jk", "<ESC>", opts)                    -- Press jk fast to exit insert mode
+keymap("n", "<Enter>", "<cmd>nohlsearch<CR>", opts) -- Clear search
+keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+keymap("n", "<leader>mx", "<cmd>!chmod +x %<CR>", { desc = "Chmod +x without leaving document", silent = true })
 
-        vim.keymap.set('n', '<space>fg', function()
-            local opts = require('telescope.themes').get_cursor()
-            builtin.live_grep(opts)
-        end, { desc = 'Telescope live grep' })
+keymap("n", "<leader>ms", ":%s/", { desc = "multi select & replace" }) -- Easier multi select and remove
+vim.keymap.set("v", "<leader>ms", [[:s//g<Left><Left>]],
+    { desc = "Multi-select & replace", noremap = true, silent = true })
 
-        -- vim.keymap.set('n', '<space>fd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
-        -- vim.keymap.set('n', '<space>fr', builtin.oldfiles, { desc = 'Telescope recent files' })
-        -- vim.keymap.set('n', '<space>ft', builtin.treesitter, { desc = 'Telescope treesitter' })
-        vim.keymap.set('n', '<space>,', builtin.buffers, { desc = 'Telescope buffers' })
+keymap("v", "J", ":m '>+1<CR>gv=gv")                                              -- move selected lines DOWN
+keymap("v", "K", ":m '<-2<CR>gv=gv")                                              -- move selected lines UP
 
-        -- Git-related pickers
-        -- vim.keymap.set('n', '<space>fgb', builtin.git_branches, { desc = 'Telescope git branches' })
-        -- vim.keymap.set('n', '<space>fgc', builtin.git_commits, { desc = 'Telescope git commits' })
-        -- vim.keymap.set('n', '<space>fgs', builtin.git_status, { desc = 'Telescope git status' })
-    end,
-}
+keymap("n", "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true }) -- Allow moving the cursor through wrapped lines
+keymap("n", "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true }) -- Allow moving the cursor through wrapped lines
+
+keymap("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true })
+
+
+local function run_python_in_tmux()
+    local file = vim.fn.expand("%:p")
+    local cmd = string.format(
+        "tmux split-window -h 'clear; python3 \"%s\"; echo; echo Press enter to exit...; read'",
+        file
+    )
+    vim.fn.system(cmd)
+end
+
+vim.keymap.set("n", "<leader>rp", run_python_in_tmux, { desc = "Run current Python file in right tmux split" })
