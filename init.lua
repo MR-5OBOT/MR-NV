9ddeb2d711cbd58ee0703157d706b6727324d0ad
MR-5OBOT <yassir7m7@gmail.com>
2025-04-22 00:15:21 +0100

    .

--- init.lua
+++ init.lua
@@ -11,14 +11,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- -- Load options first (basic Neovim settings)
+-- Load options first (basic Neovim settings)
 require("config.options")
---
--- -- Initialize lazy.nvim and load plugins
+-- Initialize lazy.nvim and load plugins
 require("config.lazy-cfg")
---
--- -- Load keymaps after plugins (so plugin keymaps don't override custom ones)
+-- Load keymaps after plugins (so plugin keymaps don't override custom ones)
 require("config.keymaps")
---
--- -- Load autocommands last
+-- Load autocommands last
 require("config.autocommands")
