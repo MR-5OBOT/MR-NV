ce4e77f025288ddfae5eb6299e93a063b609aa8a
MR-5OBOT <yassir7m7@gmail.com>
2025-04-29 03:54:25 +0100

    add termfloat keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,12 +19,7 @@ keymap("i", "jk", "<ESC>", opts) -- Press jk fast to exit insert mode
 keymap("n", "<Enter>", "<cmd>nohlsearch<CR>", opts) -- Clear search
 keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
 keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Next buffer" })
-keymap(
-  "n",
-  "<leader>mx",
-  "<cmd>!chmod +x %<CR>",
-  { desc = "Chmod +x without leaving document", silent = true }
-)
+keymap("n", "<leader>mx", "<cmd>!chmod +x %<CR>", { desc = "Chmod +x without leaving document", silent = true })
 
 keymap("n", "<leader>ms", ":%s/", { desc = "multi select & replace" }) -- Easier multi select and remove
 vim.keymap.set(
@@ -42,18 +37,15 @@ keymap("n", "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true }
 
 keymap("n", "<leader>z", ":Td<CR>", { silent = true })
 
+-- run pthon files
 local function run_python_in_tmux()
   local file = vim.fn.expand("%:p")
-  local cmd = string.format(
-    "tmux split-window -h 'clear; python3 \"%s\"; echo; echo Press enter to exit...; read'",
-    file
-  )
+  local cmd =
+    string.format("tmux split-window -h 'clear; python3 \"%s\"; echo; echo Press enter to exit...; read'", file)
   vim.fn.system(cmd)
 end
+keymap("n", "<leader>rp", run_python_in_tmux, { desc = "Run current Python file in right tmux split" })
 
-vim.keymap.set(
-  "n",
-  "<leader>rp",
-  run_python_in_tmux,
-  { desc = "Run current Python file in right tmux split" }
-)
+-- easy float term
+local terminal = require("custom-plugins.floatTerm")
+vim.keymap.set("n", "<C-\\>", terminal.toggle_terminal, { desc = "Toggle floating terminal" })
