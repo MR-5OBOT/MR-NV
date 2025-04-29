00454e98860105183c9ebfe7e03f9eeb1c5f2c49
MR-5OBOT <yassir7m7@gmail.com>
2025-04-29 03:54:33 +0100

    be aware

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,61 +5,61 @@ local opt = vim.opt
 opt.updatetime = 100 -- Faster completion
 opt.timeoutlen = 300 -- Faster key sequence completion
 opt.redrawtime = 1500 -- Allow more time for loading syntax
-opt.synmaxcol = 240 -- Only highlight the first 240 columns
-opt.maxmempattern = 2000 -- Increase the max memory for pattern matching
-opt.foldmethod = "manual" -- you can go lighter than 'indent'
-opt.foldlevelstart = 99 -- Don't fold by default
-opt.foldenable = false -- Disable folding by default
-opt.foldcolumn = "0" -- Don't show fold column
-opt.foldlevel = 99 -- Don't fold by default
-opt.foldopen = "block,hor,insert,jump,mark,percent,quickfix,search,tag,undo" -- Don't fold when opening
-opt.foldclose = "all" -- Fold when closing
-opt.foldminlines = 1 -- Fold even single line
-opt.foldnestmax = 20 -- Maximum nesting level
-opt.foldtext = "" -- Don't show fold text
-opt.foldignore = "" -- Don't ignore anything when folding
-vim.opt.breakindent = true
+opt.synmaxcol = 240 -- Only highlight first 240 columns (performance)
+opt.maxmempattern = 2000 -- Increase max memory for pattern matching
 
--- Existing options
-opt.clipboard = "unnamedplus" -- Use system clipboard
-opt.number = true -- Show absolute line numbers
-opt.relativenumber = true -- Show relative line numbers
-opt.termguicolors = true -- Enable true colors
-opt.autoindent = true -- Auto-indent new lines
-opt.shiftwidth = 4 -- Indentation level
-opt.tabstop = 4 -- Tab width
-opt.expandtab = true -- Use spaces instead of tabs
-opt.ignorecase = true -- Ignore case in search
-opt.smartcase = true -- Enable smart case search
-opt.hlsearch = true -- Highlight search matches
-opt.splitbelow = true -- Horizontal splits go below
-opt.splitright = true -- Vertical splits go right
-opt.scrolloff = 8 -- Lines to keep above/below the cursor
-opt.sidescrolloff = 8 -- Columns to keep left/right of the cursor
-opt.backup = false -- Disable backups
-opt.undofile = true -- Enable persistent undo
--- opt.inccommand = "split" -- split find and replace
+-- Folding (optional, but lightweight)
+opt.foldmethod = "manual"
+opt.foldlevel = 99
+opt.foldenable = false
+
+vim.opt.breakindent = true -- Better wrapping visuals (very cheap)
+
+-- Essential UI and behavior
+opt.clipboard = "unnamedplus" -- System clipboard
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+opt.autoindent = true
+opt.smartindent = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.expandtab = true
+
+-- Searching
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+
+-- Window behavior
+opt.splitbelow = true
+opt.splitright = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+-- Files
+opt.backup = false
 opt.swapfile = false
-opt.history = 50 -- Increased command history size
-opt.linebreak = true -- Don't split words when wrapping
-opt.wrap = false -- Disable line wrapping
-opt.cursorline = true -- Highlight current line
-opt.fileencoding = "utf-8" -- File encoding
+opt.undofile = true
+opt.history = 50
+opt.fileencoding = "utf-8"
 vim.opt.encoding = "utf-8"
-opt.showtabline = 0 -- Disable tabline
-opt.laststatus = 0
-opt.smartindent = true -- Enable smart indentation
 
--- Modern conveniences
-opt.mouse = "a" -- Enable mouse in all modes
-opt.signcolumn = "yes" -- Always show sign column
-opt.completeopt = { "menuone", "noselect" } -- Better completion experience
-opt.wildmode = "longest:full,full" -- Better command-line completion
-opt.wildmenu = true -- Enable wild menu
-opt.wildignorecase = true -- Ignore case in wild menu
-opt.pumheight = 8 -- Maximum number of items in completion menu
+-- UI niceties
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.mouse = "a"
 
--- Disable built-in plugins
+-- Completion
+opt.completeopt = { "menuone", "noselect" }
+opt.pumheight = 8
+
+-- Command-line experience
+opt.wildmode = "longest:full,full"
+opt.wildmenu = true
+opt.wildignorecase = true
+
+-- Disable built-in plugins (important for speed)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_2html_plugin = 1
