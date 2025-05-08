175e5c74b0695af32896c3a1019505a9a15eaef4
MR-5OBOT <yassir7m7@gmail.com>
2025-05-08 02:37:09 +0100

    .

--- lua/config/lazy-cfg.lua
+++ lua/config/lazy-cfg.lua
@@ -8,43 +8,43 @@
 -- Lazy.nvim Setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    lazyrepo,
-    lazypath,
-  })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup Lazy.nvim with plugins
 require("lazy").setup({
-  { import = "plugins" },
-  { import = "plugins.lsp" },
-  { import = "plugins.helpers" },
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  { "folke/neodev.nvim" },
-  { "nvim-tree/nvim-web-devicons" },
+    { import = "plugins" },
+    { import = "plugins.lsp" },
+    { import = "plugins.helpers" },
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    { "folke/neodev.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
 }, {
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+        cache = {
+            enabled = true,
+            path = vim.fn.stdpath("cache") .. "/lazy/cache",
+            -- Don't cache files larger than 100kb
+            size = 100 * 1024,
+        },
     },
-    cache = {
-      enabled = true,
-      path = vim.fn.stdpath("cache") .. "/lazy/cache",
-      -- Don't cache files larger than 100kb
-      size = 100 * 1024,
-    },
-  },
 })
