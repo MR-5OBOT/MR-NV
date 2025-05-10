b14357a63b5f0c6efbef9f85e34a0feee33e15cb
MR-5OBOT <yassir7m7@gmail.com>
2025-05-10 21:32:02 +0100

    mark this for ready to code not chasing shit

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -1,41 +1,42 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        vim.g.disable_autoformat = false
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua", stop_after_first = true },
-                python = { "black" },
-                javascript = { "prettier", stop_after_first = true },
-                html = { "prettier" },
-                json = { "prettier" },
-                markdown = { "prettier" },
-                css = { "prettier", stop_after_first = true },
-            },
-            formatters = {
-                black = {
-                    args = { "--line-length", "130" }, -- Set max line length to 130
-                },
-            },
-            format_on_save = function(bufnr)
-                if vim.g.disable_autoformat then
-                    return
-                end
-                return {
-                    timeout_ms = 500,
-                    lsp_format = "fallback",
-                }
-            end,
-        })
+	"stevearc/conform.nvim",
+	config = function()
+		vim.g.disable_autoformat = false
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua", stop_after_first = true },
+				python = { "black" },
+				javascript = { "prettier", stop_after_first = true },
+				html = { "prettier" },
+				json = { "prettier" },
+				markdown = { "prettier" },
+				css = { "prettier", stop_after_first = true },
+			},
+			formatters = {
+				black = {
+					args = { "--line-length", "130", "$FILENAME" },
+					stdin = false, -- Pass filename instead of stdin
+				},
+			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat then
+					return
+				end
+				return {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
+			end,
+		})
 
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function(args)
-                if vim.g.disable_autoformat then
-                    return
-                end
-                require("conform").format({ bufnr = args.buf })
-            end,
-        })
-    end,
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				if vim.g.disable_autoformat then
+					return
+				end
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,82 +1,96 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			-- "j-hui/fidget.nvim", -- Optional: Uncomment if you want LSP status
+			"hrsh7th/cmp-nvim-lsp", -- for capabilities
+		},
+		lazy = false,
+		config = function()
+			-- Mason setup
+			require("mason").setup()
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "bashls",
-                    "lua_ls",
-                    "html",
-                    "cssls",
-                    "pylsp",
-                },
-            })
-        end,
-    },
+			-- Formatter & tool installer
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					"black",
+					"prettier",
+				},
+				auto_update = false,
+				run_on_start = true,
+				start_delay = 3000,
+			})
 
-    {
-        "neovim/nvim-lspconfig",
-        -- dependencies = { "j-hui/fidget.nvim" },
-        config = function()
-            -- require("fidget").setup({})
+			-- LSP servers to install
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"bashls",
+					"lua_ls",
+					"html",
+					"cssls",
+					"pylsp",
+				},
+			})
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+			-- Optional: status display
+			-- require("fidget").setup({})
 
-            local function on_attach(_, bufnr)
-                local opts = { noremap = true, silent = true, buffer = bufnr }
-                local keymap = vim.keymap.set
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-                keymap("n", "gd", vim.lsp.buf.definition, opts)
-                keymap("n", "gD", vim.lsp.buf.declaration, opts)
-                keymap("n", "gi", vim.lsp.buf.implementation, opts)
-                keymap("n", "gr", vim.lsp.buf.references, opts)
-                keymap("n", "K", vim.lsp.buf.hover, opts)
-                keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-                keymap("n", "[d", vim.diagnostic.goto_prev, opts)
-                keymap("n", "]d", vim.diagnostic.goto_next, opts)
-                keymap("n", "<leader>e", vim.diagnostic.open_float, opts)
-                keymap("n", "<leader>q", vim.diagnostic.setloclist, opts)
-            end
+			local function on_attach(_, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+				local keymap = vim.keymap.set
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
+				keymap("n", "gd", vim.lsp.buf.definition, opts)
+				keymap("n", "gD", vim.lsp.buf.declaration, opts)
+				keymap("n", "gi", vim.lsp.buf.implementation, opts)
+				keymap("n", "gr", vim.lsp.buf.references, opts)
+				keymap("n", "K", vim.lsp.buf.hover, opts)
+				keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				keymap("n", "[d", vim.diagnostic.goto_prev, opts)
+				keymap("n", "]d", vim.diagnostic.goto_next, opts)
+				keymap("n", "<leader>q", vim.diagnostic.setloclist, opts)
+			end
 
-            for _, server in ipairs({ "bashls", "cssls", "html", "pylsp" }) do
-                lspconfig[server].setup({
-                    capabilities = capabilities,
-                    on_attach = on_attach,
-                })
-            end
+			-- Lua LS
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
 
-            vim.diagnostic.config({
-                virtual_text = true,
-                signs = true,
-                float = { border = "rounded" },
-            })
-        end,
-    },
+			-- Other LSPs
+			for _, server in ipairs({ "bashls", "cssls", "html", "pylsp" }) do
+				lspconfig[server].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+			end
+
+			-- Diagnostics UI
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				float = { border = "rounded" },
+			})
+		end,
+	},
 }
