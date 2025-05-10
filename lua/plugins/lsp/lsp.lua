5cabcd17d57083b2ebcb46d0667e45f588f53615
MR-5OBOT <yassir7m7@gmail.com>
2025-05-10 05:25:05 +0100

    i hope

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,139 +1,102 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                PATH = "prepend",
-            })
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "bashls",
-                    "lua_ls",
-                    "html",
-                    "cssls",
-                    "ts_ls",
-                    "pylsp",
-                    "black",
-                    "stylua"
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        { "j-hui/fidget.nvim", opts = {}, },
-        config = function()
-            -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
-            local lspconfig = require("lspconfig")
-            local configs = require("lspconfig.configs")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				PATH = "prepend",
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"bashls",
+					"lua_ls",
+					"html",
+					"cssls",
+					"ts_ls",
+					"pylsp",
+					"black",
+					"stylua",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		{ "j-hui/fidget.nvim", opts = {} },
+		config = function()
+			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local lspconfig = require("lspconfig")
+			local configs = require("lspconfig.configs")
 
-            lspconfig.bashls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                -- cmd = { "lua_ls" },
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" }, -- Recognize 'vim' as a global variable
-                        },
-                        workspace = {
-                            library = {
-                                vim.api.nvim_get_runtime_file("", true),
-                                "${3rd}/love2d/library",
-                            }, -- Include Neovim runtime files
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-                filetypes = {
-                    "templ",
-                    "html",
-                    "php",
-                    "css",
-                    "javascriptreact",
-                    "typescriptreact",
-                    "javascript",
-                    "typescript",
-                    "jsx",
-                    "tsx",
-                },
-            })
-            lspconfig.htmx.setup({
-                capabilities = capabilities,
-                filetypes = { "html", "templ" },
-            })
-            if not configs.ts_ls then
-                configs.ts_ls = {
-                    default_config = {
-                        cmd = { "typescript-language-server", "--stdio" },
-                        capabilties = capabilities,
-                        filetypes = {
-                            "javascript",
-                            "javascriptreact",
-                            "typescript",
-                            "typescriptreact",
-                            "html",
-                        },
-                        root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", ".git"),
-                        -- single_file_support = true,
-                    },
-                }
-            end
-            lspconfig.ts_ls.setup({
-                -- capabilties = capabilities,
-            })
-            lspconfig.eslint.setup({
-                capabilties = capabilities,
-            })
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" }, -- Recognize 'vim' as a global variable
+						},
+						workspace = {
+							library = {
+								vim.api.nvim_get_runtime_file("", true),
+								"${3rd}/love2d/library",
+							}, -- Include Neovim runtime files
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
 
-            function get_python_path()
-                -- Check if there's an active virtual environment
-                local venv_path = os.getenv("VIRTUAL_ENV")
-                if venv_path then
-                    return venv_path .. "/bin/python3"
-                else
-                    return "/usr/bin/python3"
-                end
-            end
+			function get_python_path()
+				-- Check if there's an active virtual environment
+				local venv_path = os.getenv("VIRTUAL_ENV")
+				if venv_path then
+					return venv_path .. "/bin/python3"
+				else
+					return "/usr/bin/python3"
+				end
+			end
 
-            lspconfig.pylsp.setup({
-                capabilties = capabilities,
-                settings = {
-                    python = {
-                        pythonPath = get_python_path(),
-                    },
-                },
-            })
-        end,
-    },
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+				settings = {
+					python = {
+						pythonPath = get_python_path(),
+					},
+				},
+			})
+		end,
+	},
 
-    -- Diagnostics
-    vim.diagnostic.config({
-        virtual_text = true,
-        signs = {
-            text = {
-                [vim.diagnostic.severity.ERROR] = "",
-                [vim.diagnostic.severity.WARN] = "",
-                [vim.diagnostic.severity.INFO] = "",
-                [vim.diagnostic.severity.HINT] = "",
-            },
-        },
-        float = { border = "rounded" },
-    })
+	-- Diagnostics
+	vim.diagnostic.config({
+		virtual_text = true,
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN] = "",
+				[vim.diagnostic.severity.INFO] = "",
+				[vim.diagnostic.severity.HINT] = "",
+			},
+		},
+		float = { border = "rounded" },
+	}),
 }
