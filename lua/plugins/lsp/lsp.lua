194ea37225d5111978739fb1c702cc2b3fc6812c
MR-5OBOT <yassir7m7@gmail.com>
2025-05-10 05:28:42 +0100

    .

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -28,45 +28,29 @@ return {
 		"neovim/nvim-lspconfig",
 		{ "j-hui/fidget.nvim", opts = {} },
 		config = function()
-			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 			local lspconfig = require("lspconfig")
 			local configs = require("lspconfig.configs")
 
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" }, -- Recognize 'vim' as a global variable
-						},
-						workspace = {
-							library = {
-								vim.api.nvim_get_runtime_file("", true),
-								"${3rd}/love2d/library",
-							}, -- Include Neovim runtime files
-						},
-						telemetry = {
-							enable = false,
-						},
-					},
-				},
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+			-- LSP keymaps
+			local function on_attach(_, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+				local keymap = vim.keymap.set
 
-			function get_python_path()
-				-- Check if there's an active virtual environment
+				keymap("n", "gd", vim.lsp.buf.definition, opts)
+				keymap("n", "gD", vim.lsp.buf.declaration, opts)
+				keymap("n", "gi", vim.lsp.buf.implementation, opts)
+				keymap("n", "gr", vim.lsp.buf.references, opts)
+				keymap("n", "K", vim.lsp.buf.hover, opts)
+				keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				keymap("n", "[d", vim.diagnostic.goto_prev, opts)
+				keymap("n", "]d", vim.diagnostic.goto_next, opts)
+				keymap("n", "<leader>e", vim.diagnostic.open_float, opts)
+				keymap("n", "<leader>q", vim.diagnostic.setloclist, opts)
+			end
+
+			local function get_python_path()
 				local venv_path = os.getenv("VIRTUAL_ENV")
 				if venv_path then
 					return venv_path .. "/bin/python3"
@@ -75,8 +59,50 @@ return {
 				end
 			end
 
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = {
+								vim.api.nvim_get_runtime_file("", true),
+								"${3rd}/love2d/library",
+							},
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			lspconfig.html.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
 			lspconfig.pylsp.setup({
 				capabilities = capabilities,
+				on_attach = on_attach,
 				settings = {
 					python = {
 						pythonPath = get_python_path(),
