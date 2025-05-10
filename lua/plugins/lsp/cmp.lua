69a7fe66ca4eb2db9d109ec75e4faaf88cedb3b7
MR-5OBOT <yassir7m7@gmail.com>
2025-05-10 20:45:42 +0100

    bro it just work fuck perfection lol

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,152 +1,140 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter", -- Lazy load on insert
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"saadparwaiz1/cmp_luasnip",
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = {
-				"rafamadriz/friendly-snippets",
-			},
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-			end,
-		},
-		"onsails/lspkind.nvim",
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
+        {
+            "L3MON4D3/LuaSnip",
+            dependencies = {
+                "rafamadriz/friendly-snippets",
+            },
+            config = function()
+                require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+        },
+        "onsails/lspkind.nvim",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
 
-		-- Load snippets
-		require("luasnip.loaders.from_vscode").lazy_load()
+        local function has_words_before()
+            local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
 
-		-- Better mapping function with fallback
-		local function has_words_before()
-			local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({
+                    select = true,
+                    behavior = cmp.ConfirmBehavior.Replace,
+                }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp", max_item_count = 12, priority = 10 },
+                { name = "luasnip",  max_item_count = 8,  priority = 7 },
+                { name = "buffer",   max_item_count = 5,  priority = 5, keyword_length = 3 },
+                { name = "path",     max_item_count = 5,  priority = 4 },
+            }),
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = "symbol_text",
+                    maxwidth = 50,
+                    ellipsis_char = "...",
+                    menu = {
+                        nvim_lsp = "[LSP]",
+                        luasnip = "[Snip]",
+                        buffer = "[Buf]",
+                        path = "[Path]",
+                    },
+                    before = function(entry, vim_item)
+                        vim_item.dup = ({ nvim_lsp = 0, luasnip = 0, buffer = 0, path = 0 })[entry.source.name] or 0
+                        return vim_item
+                    end,
+                }),
+            },
+            window = {
+                completion = cmp.config.window.bordered({
+                    border = "rounded",
+                    winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+                    scrollbar = false,
+                }),
+                documentation = cmp.config.window.bordered({
+                    border = "rounded",
+                    winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+                }),
+            },
+            performance = {
+                debounce = 30,
+                throttle = 15,
+                fetching_timeout = 80,
+                async_budget = 1,
+                max_view_entries = 12,
+            },
+            sorting = {
+                priority_weight = 2,
+                comparators = {
+                    cmp.config.compare.offset,
+                    cmp.config.compare.exact,
+                    cmp.config.compare.score,
+                    cmp.config.compare.recently_used,
+                    cmp.config.compare.kind,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order,
+                },
+            },
+            experimental = {
+                ghost_text = false,
+            },
+        })
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-u>"] = cmp.mapping.scroll_docs(-4),
-				["<C-d>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({
-					select = true,
-					behavior = cmp.ConfirmBehavior.Replace,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp", max_item_count = 12, priority = 10 },
-				{ name = "luasnip", max_item_count = 8, priority = 7 },
-				{ name = "buffer", max_item_count = 5, priority = 5, keyword_length = 3 },
-				{ name = "path", max_item_count = 5, priority = 4 },
-			}),
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol_text",
-					maxwidth = 50,
-					ellipsis_char = "...",
-					menu = {
-						nvim_lsp = "[LSP]",
-						luasnip = "[Snip]",
-						buffer = "[Buf]",
-						path = "[Path]",
-					},
-					before = function(entry, vim_item)
-						-- Remove duplicates
-						vim_item.dup = ({ nvim_lsp = 0, luasnip = 0, buffer = 0, path = 0 })[entry.source.name] or 0
-						return vim_item
-					end,
-				}),
-			},
-			window = {
-				completion = cmp.config.window.bordered({
-					border = "rounded",
-					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-					scrollbar = false,
-				}),
-				documentation = cmp.config.window.bordered({
-					border = "rounded",
-					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-				}),
-			},
-			matching = {
-				disallow_fuzzy_matching = false,
-				disallow_partial_matching = false,
-				disallow_prefix_unmatching = false,
-			},
-			performance = {
-				debounce = 30,
-				throttle = 15,
-				fetching_timeout = 80,
-				async_budget = 1,
-				max_view_entries = 12,
-			},
-			sorting = {
-				priority_weight = 2,
-				comparators = {
-					cmp.config.compare.offset,
-					cmp.config.compare.exact,
-					cmp.config.compare.score,
-					cmp.config.compare.recently_used,
-					cmp.config.compare.kind,
-					cmp.config.compare.length,
-					cmp.config.compare.order,
-				},
-			},
-			experimental = {
-				ghost_text = false, -- Set to true if you want inline ghost text
-			},
-		})
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" },
+                { name = "cmdline", max_item_count = 20 },
+            }),
+        })
 
-		-- Set up special configuration for command line
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-				{ name = "cmdline", max_item_count = 20 },
-			}),
-		})
-
-		-- Set up special configuration for search
-		cmp.setup.cmdline("/", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer", max_item_count = 10 },
-			},
-		})
-	end,
+        cmp.setup.cmdline("/", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "buffer", max_item_count = 10 },
+            },
+        })
+    end,
 }
--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,115 +1,82 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				PATH = "prepend",
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"bashls",
-					"lua_ls",
-					"html",
-					"cssls",
-					"ts_ls",
-					"pylsp",
-					"black",
-					"stylua",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		{ "j-hui/fidget.nvim", opts = {} },
-		config = function()
-			local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
-			local lspconfig = require("lspconfig")
-			local configs = require("lspconfig.configs")
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
 
-			-- LSP keymaps
-			local function on_attach(_, bufnr)
-				local opts = { noremap = true, silent = true, buffer = bufnr }
-				local keymap = vim.keymap.set
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "bashls",
+                    "lua_ls",
+                    "html",
+                    "cssls",
+                    "pylsp",
+                },
+            })
+        end,
+    },
 
-				keymap("n", "gd", vim.lsp.buf.definition, opts)
-				keymap("n", "gD", vim.lsp.buf.declaration, opts)
-				keymap("n", "gi", vim.lsp.buf.implementation, opts)
-				keymap("n", "gr", vim.lsp.buf.references, opts)
-				keymap("n", "K", vim.lsp.buf.hover, opts)
-				keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-				keymap("n", "[d", vim.diagnostic.goto_prev, opts)
-				keymap("n", "]d", vim.diagnostic.goto_next, opts)
-				keymap("n", "<leader>e", vim.diagnostic.open_float, opts)
-				keymap("n", "<leader>q", vim.diagnostic.setloclist, opts)
-			end
+    {
+        "neovim/nvim-lspconfig",
+        -- dependencies = { "j-hui/fidget.nvim" },
+        config = function()
+            -- require("fidget").setup({})
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							library = {
-								vim.api.nvim_get_runtime_file("", true),
-								"${3rd}/love2d/library",
-							},
-						},
-						telemetry = {
-							enable = false,
-						},
-					},
-				},
-			})
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
+            local function on_attach(_, bufnr)
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+                local keymap = vim.keymap.set
 
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
+                keymap("n", "gd", vim.lsp.buf.definition, opts)
+                keymap("n", "gD", vim.lsp.buf.declaration, opts)
+                keymap("n", "gi", vim.lsp.buf.implementation, opts)
+                keymap("n", "gr", vim.lsp.buf.references, opts)
+                keymap("n", "K", vim.lsp.buf.hover, opts)
+                keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                keymap("n", "[d", vim.diagnostic.goto_prev, opts)
+                keymap("n", "]d", vim.diagnostic.goto_next, opts)
+                keymap("n", "<leader>e", vim.diagnostic.open_float, opts)
+                keymap("n", "<leader>q", vim.diagnostic.setloclist, opts)
+            end
 
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            })
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
+            for _, server in ipairs({ "bashls", "cssls", "html", "pylsp" }) do
+                lspconfig[server].setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                })
+            end
 
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {},
-			})
-		end,
-	},
-
-	-- Diagnostics
-	vim.diagnostic.config({
-		virtual_text = true,
-		signs = {
-			text = {
-				[vim.diagnostic.severity.ERROR] = "",
-				[vim.diagnostic.severity.WARN] = "",
-				[vim.diagnostic.severity.INFO] = "",
-				[vim.diagnostic.severity.HINT] = "",
-			},
-		},
-		float = { border = "rounded" },
-	}),
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                float = { border = "rounded" },
+            })
+        end,
+    },
 }
