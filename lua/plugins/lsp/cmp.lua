0ef44163b84a97326e67d0858f37421b3a2d2046
MR-5OBOT <yassir7m7@gmail.com>
2025-05-12 04:16:33 +0100

    adding cmp under test

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,137 +1,137 @@
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "saadparwaiz1/cmp_luasnip",
-        "L3MON4D3/LuaSnip",
-        "rafamadriz/friendly-snippets",
-        "onsails/lspkind.nvim",
-    },
-    config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
-        local lspkind = require("lspkind")
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    "onsails/lspkind.nvim",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
 
-        require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load()
 
-        local function has_words_before()
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-        end
+    local function has_words_before()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
 
-        -- Define a softer highlight for the selected item
-        vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#333333", fg = "#d0d0d0", bold = false })
+    -- Define a softer highlight for the selected item
+    vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#333333", fg = "#d0d0d0", bold = false })
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({
-                    select = true,
-                    behavior = cmp.ConfirmBehavior.Replace,
-                }),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp", max_item_count = 12, priority = 10 },
-                { name = "luasnip",  max_item_count = 8,  priority = 7 },
-                { name = "buffer",   max_item_count = 5,  priority = 5, keyword_length = 2 },
-                { name = "path",     max_item_count = 5,  priority = 4 },
-            }),
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = "symbol_text",
-                    maxwidth = 50,
-                    ellipsis_char = "...",
-                    menu = {
-                        nvim_lsp = "[LSP]",
-                        luasnip = "[Snip]",
-                        buffer = "[Buf]",
-                        path = "[Path]",
-                    },
-                    before = function(entry, vim_item)
-                        vim_item.dup = ({ nvim_lsp = 0, luasnip = 0, buffer = 0, path = 0 })[entry.source.name] or 0
-                        return vim_item
-                    end,
-                }),
-            },
-            window = {
-                completion = cmp.config.window.bordered({
-                    border = "single",
-                    winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:PmenuSel,Search:None",
-                    scrollbar = false,
-                }),
-                documentation = cmp.config.window.bordered({
-                    border = "single",
-                    winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:PmenuSel,Search:None",
-                }),
-            },
-            performance = {
-                debounce = 50,
-                throttle = 30,
-                fetching_timeout = 80,
-                async_budget = 1,
-                max_view_entries = 12,
-            },
-            sorting = {
-                priority_weight = 2,
-                comparators = {
-                    cmp.config.compare.offset,
-                    cmp.config.compare.exact,
-                    cmp.config.compare.score,
-                    cmp.config.compare.recently_used,
-                    cmp.config.compare.kind,
-                },
-            },
-            experimental = {
-                ghost_text = false,
-            },
-        })
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({
+          select = true,
+          behavior = cmp.ConfirmBehavior.Replace,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp", max_item_count = 12, priority = 10 },
+        { name = "luasnip",  max_item_count = 8,  priority = 7 },
+        { name = "buffer",   max_item_count = 5,  priority = 5, keyword_length = 2 },
+        { name = "path",     max_item_count = 5,  priority = 4 },
+      }),
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          maxwidth = 50,
+          ellipsis_char = "...",
+          menu = {
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snip]",
+            buffer = "[Buf]",
+            path = "[Path]",
+          },
+          before = function(entry, vim_item)
+            vim_item.dup = ({ nvim_lsp = 0, luasnip = 0, buffer = 0, path = 0 })[entry.source.name] or 0
+            return vim_item
+          end,
+        }),
+      },
+      window = {
+        completion = cmp.config.window.bordered({
+          border = "single",
+          winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:PmenuSel,Search:None",
+          scrollbar = false,
+        }),
+        documentation = cmp.config.window.bordered({
+          border = "single",
+          winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:PmenuSel,Search:None",
+        }),
+      },
+      performance = {
+        debounce = 50,
+        throttle = 30,
+        fetching_timeout = 80,
+        async_budget = 1,
+        max_view_entries = 12,
+      },
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.kind,
+        },
+      },
+      experimental = {
+        ghost_text = false,
+      },
+    })
 
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" },
-                { name = "cmdline", max_item_count = 20 },
-            }),
-        })
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+        { name = "cmdline", max_item_count = 20 },
+      }),
+    })
 
-        cmp.setup.cmdline("/", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "buffer",   max_item_count = 10 },
-                { name = "nvim_lsp", max_item_count = 5 },
-            }),
-        })
-    end,
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "buffer",   max_item_count = 10 },
+        { name = "nvim_lsp", max_item_count = 5 },
+      }),
+    })
+  end,
 }
