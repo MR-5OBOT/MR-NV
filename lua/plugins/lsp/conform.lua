b9b62ca162fe328355ca1ac2a3ee6aadd1137f41
MR-5OBOT <yassir7m7@gmail.com>
2025-06-03 18:34:57 +0100

    .

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -4,25 +4,21 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				python = { "black" },
-				-- Conform will run the first available formatter
+				python = { "ruff_format" },
 				javascript = { "prettier", stop_after_first = true },
 			},
 			format_on_save = {
-				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 				lsp_format = "fallback",
 			},
-			-- Conform will notify you when a formatter errors
 			notify_on_error = true,
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({ bufnr = args.buf })
-				end,
-			}),
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
 		})
 	end,
 }
