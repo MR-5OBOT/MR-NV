83e362ebe2f4c72bc0c99652a9fa6e0f784897b4
MR-5OBOT <yassir7m7@gmail.com>
2026-08-06 10:48:25 +0100

    .

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		vim.keymap.set("n", "K", function()
-			vim.lsp.buf.hover({ border = "single", winhighlight = "FloatBorder:Whitespace" })
+			vim.lsp.buf.hover({ border = "single", winhighlight = "FloatBorder:Whitespace", anchor_bias = "below" })
 		end, { buffer = ev.buf, silent = true, desc = "Hover documentation" })
 		if client ~= nil and client:supports_method("textDocument/completion") then
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
