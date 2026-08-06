7bdefba29eebb2c8f53d80049ea1d4595da4adb4
MR-5OBOT <yassir7m7@gmail.com>
2026-08-06 09:03:25 +0100

    cleaning

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,10 +1,3 @@
--- Write and quit
-vim.keymap.set("n", "<leader>w", ":w<cr>", { silent = true })
-vim.keymap.set("n", "<leader>q", ":q<cr>", { silent = true })
-
--- Redo
-vim.keymap.set("n", "U", "<c-r>", { silent = true })
-
 -- Swap between split buffers
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { silent = true, desc = "Move to left split" })
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true, desc = "Move to below split" })
